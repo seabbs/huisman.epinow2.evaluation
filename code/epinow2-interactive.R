f58cb39c28ece7c45200beecb744456d84919925
seabbs <s.e.abbott12@gmail.com>
2021-01-04 12:09:08 +0000

    tweaks to deal with simulated data issues

--- code/epinow2-interactive.R
+++ code/epinow2-interactive.R
@@ -16,9 +16,10 @@ simulated_cases <- simulated_cases[,.(date, confirm = observations)]
 # Add poisson noise 
 # Huisman et al simulation used above is deterministic outside of delays from
 # infection to observation (critically it has no error model for infections or 
-# observations). This causes EpiNow2 issues as it is designed to fit to real world 
-# data where observation error is likely to be present.
-simulated_cases$confirm <- map_dbl(simulated_cases$confirm, ~ rpois(1, .))
+# observations and noise used when sampling from delays).
+# This causes EpiNow2 issues as it is designed to fit to real world data where
+# observation error is likely to be present.
+simulated_cases <- simulated_cases[, confirm := map_dbl(confirm, ~ rpois(1, .))]
 
 # Estimate delays from gamma distributions used for simulation
 # EpiNow2 is designed to work with only lognormal delays currently 
@@ -28,7 +29,7 @@ simulated_cases$confirm <- map_dbl(simulated_cases$confirm, ~ rpois(1, .))
 # the Huisman et al. code to work with log normals
 approximate_lognormal <- function(param_def) {
   estimate_delay(rgamma(1000, shape =  param_def$shape, scale = param_def$scale),
-                 bootstraps = 10, bootstrap_samples = 100, max_value = 15)
+                 bootstraps = 1, bootstrap_samples = 1000, max_value = 15)
 }
 incubation_period <- approximate_lognormal(IncubationParams)
 reporting_delay <-  approximate_lognormal(OnsetToCountParams)
@@ -38,21 +39,23 @@ reporting_delay <-  approximate_lognormal(OnsetToCountParams)
 # as the Huisman et al approach does not support uncertainty a small placeholder has been used
 generation_time <- list(
   mean = 4.8,
-  mean_sd = 0.01,
+  mean_sd = 0.05,
   sd = 2.3,
-  sd_sd = 0.01,
+  sd_sd = 0.05,
   max = 15
 )
 
 # set defaults for EpiNow2
 # here we use no day of the week effect in line with the Huisman et al
 # (as this is also built into their data simulation) approach and a Poisson 
-# observation (introduced above to add stochasticity to their simulation). 
+# observation (introduced above to add stochasticity to the Huisman et al simulation). 
+# here we reduce the control parameters used for stan as the deterministic data is difficult for the
+# stochastic algorithm to fit to.
 estimate_with_epinow2 <- function(...) {
   estimate_infections(reported_cases = simulated_cases, 
                       generation_time = generation_time,
                       delays = delay_opts(incubation_period, reporting_delay),
-                      stan = stan_opts(control = list(adapt_delta = 0.95)),
+                      stan = stan_opts(control = list(adapt_delta = 0.9, max_treedepth = 10)),
                       obs = obs_opts(family = "poisson", week_effect = FALSE),
                       horizon = 0, verbose = TRUE, ...)
 }
@@ -60,4 +63,4 @@ estimate_with_epinow2 <- function(...) {
 # time performance (though comparable to other bayesian deconvolution approaches)
 epinow2_Re_deconvolution <- estimate_with_epinow2(rt = NULL)
 # generative approach that has a longer run time but better real time estimates
-epinow2_Re_generative <- estimate_with_epinow2(rt = rt_opts(prior = list(mean = 2.5, sd = 0.5)))
+epinow2_Re_generative <- estimate_with_epinow2(rt = rt_opts(prior = list(mean = 3, sd = 0.5)))
